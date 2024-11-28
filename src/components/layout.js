356631be4a2217e2a95d35320c28a7fc5af99961
Abdelrahman Ayman <55602893+AbdelrahmanAyman48@users.noreply.github.com>
2024-11-28 20:35:33 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,18 +4,31 @@ import Header from './header'
 import CartTab from './cartTab' 
 import { useSelector } from 'react-redux'
 
-const Layout = () => {
-    const statusTabCart = useSelector(store => store.cart.statusTab);
-  return (
-    <div className='bg-zinc-200 w-[900px] '>
-        <main className={`w-[900px] max-w-full m-auto p-5 transform transition-transform duration-500
-        ${statusTabCart === false ? "" : "-translate-x-56" }`}>
-            <Header />
-            <Outlet />
-        </main>
-        <CartTab />
-    </div>
-  )
+// const Layout = () => {
+//     const statusTabCart = useSelector(store => store.cart.statusTab);
+
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+const Component = ({ statusTabCart }) => {
+    return (
+        <div className="bg-zinc-200 w-[900px]">
+            <main
+                className={twMerge(
+                    clsx(
+                        'w-[900px] max-w-full m-auto p-5 transform transition-transform duration-500',
+                        {
+                            '-translate-x-56': statusTabCart
+                        }
+                    )
+                )}
+            >
+                <Header />
+                <Outlet />
+            </main>
+            <CartTab />
+        </div>
+    );
 }
 
 export default Layout
